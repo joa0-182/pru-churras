90b901cd0d154f66465174e30f5836ec1e674508
Jo├úo Pedro Maria <joaopedromaria182@gmail.com>
2023-03-17 20:29:43 -0300

    Fazendo o tabs 3 - Dados

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,32 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  tamanho = 6
+  valorAtual = 0
+
+  imgDados = [
+    'assets/dice-red.png',
+    'assets/dice.gif'
+  ];
+
+  imgDado = this.imgDados[0];
+
   constructor() {}
+
+  jogarDado() {
+    this.valorAtual = 0;
+    this.valorAtual = Math.floor(Math.random());
+    this.valorAtual = this.valorAtual * 6;
+
+
+    this.imgDado = this.imgDados[1]
+    setTimeout(() => {
+      this.imgDado
+    }, 950);
+
+  }
+
+
+
 
 }
